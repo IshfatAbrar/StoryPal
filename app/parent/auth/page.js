5c77536b7a35e09d73b25f3782845cab4041ea27
IshfatAbrar <ishfat0001abrar@gmail.com>
2025-12-15 03:00:53 -0500

    fixed build error- next version

--- app/parent/auth/page.js
+++ app/parent/auth/page.js
@@ -1,5 +1,8 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
 import {
